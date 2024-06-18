cbb0a847b2f5248897a136a1f479eb1fa55fcd9b
JAHONGIR-AMONOV <jahongir.amonov3010@gmail.com>
2024-06-18 11:21:39 +0500

    price of cars

---
+++ script.js
@@ -0,0 +1,38 @@
+let cars = [
+    9000,
+    14000,
+    7000,
+    30000,
+    37000,
+    55000,
+    23000,
+    75000,
+    100000
+]
+
+let cheap = cars.filter(num => num < 35000);
+
+console.log(cheap);
+
+
+
+// let arr = ['Aleksey', 'Kartoshka', 'Margarita' , 'Morgenshtern', 'Monica']
+
+// let index = prompt('Введите число')
+
+// if(index < 5) {
+//     arr.splice(index, 1)
+// } else { 
+//     alert("Такого индекса нет в списке")
+// }
+
+// console.log(arr);
+
+
+// let array = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22]
+
+// if(array > 5) {
+//     console.log(good);
+// } else {
+//     console.log(bad);
+// }
